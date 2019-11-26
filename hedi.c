e7d425f13f45f64324478ae7f1617543d4948efd
ihavetea <ihavetea@gmail.com>
2019-11-26 22:03:23 +0100

    autopush

---
+++ hedi.c
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+Wordpress
+Prestashop
+
+Reagarder les alternatives
+
+Site de e commerce
+notamment pour la compatibilite e commerce
+
+precommandes
+gestion de stock
+
+
+vins primeur
+
+il ya des siytes les vins mais pas les vins primeur
+ils vendent tous types de vins
+
+hotel resto particuliers
+compte client
+
+
+gestion des stocks complexe < vins primeur
+
+vins primeur => specificite
+
+il y a une boutique
+retrait possible
+
+
+stock vin en caissage (6 & 12 etc)
+ou a l'unite
+
+tarifs pro particuliers etc
+
+
+
+base de donnee SQL Server
+Navision microsoft
+
+
+
